9c1ac3e9ba01d3bae6589231a5ea7814c1b35f8d
vilmarti <vi.fri.martin@gmail.com>
2019-03-10 12:16:37 +0100

    some changes

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import roc_curve, auc, precision_recall_curve
 
 mapping = {'A': [1, 0, 0, 0], 
            'T': [0, 1, 0, 0],
@@ -16,3 +17,14 @@ def map_dna_into_vector(string):
         vector.append(mapping[c])
     vector = np.hstack(vector)
     return vector
+
+def get_scores(true_val, pred_val):    
+    '''
+        computes the Area under the ROC-Curve and the area under the Precision-Recall-Curve
+    '''
+    fpr, tpr, _ = roc_curve(true_val, pred_val)
+    auroc = auc(fpr, tpr)
+    
+    precision, recall, _ = precision_recall_curve(true_val, pred_val)
+    auprc = auc(recall, precision)
+    return (auroc, auprc)
