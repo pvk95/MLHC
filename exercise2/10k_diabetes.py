1e7af9fdba9520c7a7de4b6e3a401f000f44bec6
martin <vilmarti@student.ethz.ch>
2019-03-22 16:22:48 +0100

    join string and remove punctuation

--- exercise2/10k_diabetes.py
+++ exercise2/10k_diabetes.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 import sklearn
-import tensorflow as tf
+import string
 
 # Importing dataframes
 train_df = pd.read_csv('project2_data/10k_diabetes/diab_train.csv')
@@ -18,7 +20,8 @@ pred_columns = ['Unnamed: 0']
 int_columns = ['time_in_hospital', 'num_lab_procedures',
                'num_procedures', 'num_medications', 'number_outpatient',
                'number_emergency', 'number_inpatient', 'number_diagnoses',
-               'readmitted', 'diag_1', 'diag_2', 'diag_3']
+               'readmitted', 'diag_1', 'diag_2', 'diag_3'
+               ]
 
 # All categorical columns
 cat_columns = ['race', 'gender', 'age', 'weight',
@@ -34,7 +37,7 @@ cat_columns = ['race', 'gender', 'age', 'weight',
 
 # All string columns
 str_columns = ['diag_1_desc', 'diag_2_desc', 'diag_3_desc', 'payer_code',
-               'admission_source_id', 'medical_speciality', 'admission_type_id',
+               'admission_source_id', 'medical_specialty', 'admission_type_id',
                'discharge_disposition_id'
                ]
 
@@ -43,7 +46,7 @@ category_map = {'race': {'AfricanAmerican': 0, 'Caucasian': 1, 'Asian': 2, 'Othe
                 'age': {'[60-70)': 0, '[70-80)': 1, '[80-90)': 2, '[50-60)': 3, '[20-30)': 4, '[40-50)': 5, '[30-40)': 6, '[10-20)': 7, '[90-100)': 8, '[0-10)': 9},
                 'weight': {'?': np.NaN, '[75-100)': 1, '[100-125)': 2, '[50-75)': 3, '[125-150)': 4, '[25-50)': 5, '[150-175)': 6, '[0-25)': 7},
                 'max_glu_serum': {'None': np.NaN, 'Norm': 1, '>200': 2, '>300': 3},
-                'A1Cresult': {'None': np.NaN, '>8': 1, 'Norm': 2, '>7': 3},
+                'A1Cresult': {'None': 0, '>8': 1, 'Norm': 2, '>7': 3},
                 'metformin': {'No': 0, 'Steady': 1, 'Up': 2, 'Down': 3},
                 'repaglinide': {'No': 0, 'Steady': 1, 'Up': 2, 'Down': 3},
                 'nateglinide': {'No': 0, 'Steady': 1, 'Up': 2},
@@ -104,7 +107,7 @@ valid_df = fix_integers(valid_df, int_columns)
 test_df = fix_integers(test_df, int_columns)
 
 # Find category map
-#category_map = create_maps(train_df, cat_columns, NaN_values)
+# category_map = create_maps(train_df, cat_columns, NaN_values)
 # print(category_map)
 
 # Fix Categories
@@ -118,12 +121,11 @@ train_data_numerical = train_df[cat_columns + int_columns]
 valid_data_numerical = valid_df[cat_columns + int_columns]
 test_data_numerical = test_df[cat_columns + int_columns]
 
-train_y = train_df[pred_columns]
-valid_y = valid_df[pred_columns]
-test_y = test_df[pred_columns]
+# get y_values
+train_y = train_df[pred_columns[0]]
+valid_y = valid_df[pred_columns[0]]
+test_y = test_df[pred_columns[0]]
 
-
-# Simple linear regression
 
 # Fill missing values with median
 train_data_numerical = train_data_numerical.fillna(
@@ -132,7 +134,43 @@ valid_data_numerical = valid_data_numerical.fillna(
     train_data_numerical.median())
 test_data_numerical = test_data_numerical.fillna(train_data_numerical.median())
 
+##************************************************** ##
+#                Testing Linear Regression           ##
+##************************************************** ##
 
 reg = LinearRegression().fit(train_data_numerical.values, train_y)
 score = reg.score(valid_data_numerical.values, valid_y)
-print(f'The score on the validation set was {score}')
+print(f'The score on the validation set was {score:.3f}')
+
+##************************************************** ##
+#                Testing SVM                         ##
+##************************************************** ##
+
+svc = SVR(gamma='scale').fit(
+    train_data_numerical.values,
+    train_y.values.ravel())
+score = svc.score(valid_data_numerical.values, valid_y.values.ravel())
+print(f'The score on the validation set was {score:.3f}')
+
+
+##************************************************** ##
+#                String                              ##
+##************************************************** ##
+
+translator = str.maketrans('', '', string.punctuation)
+
+
+def vector_to_string(vector):
+    vector = [str(s) for s in vector]
+    joined_string = " ".join(vector)
+    string_no_punctuation = joined_string.translate(translator)
+    return string_no_punctuation
+
+
+# Join all strings together and remove punctuation
+train_data_string = np.apply_along_axis(
+    vector_to_string, 1, train_df[str_columns].values)
+valid_data_string = np.apply_along_axis(
+    vector_to_string, 1, valid_df[str_columns].values)
+test_data_string = np.apply_along_axis(
+    vector_to_string, 1, test_df[str_columns].values)
