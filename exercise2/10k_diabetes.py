e39e2b8c3121417f4e22dfb48ef9e05aabd26393
martin <vilmarti@student.ethz.ch>
2019-03-22 14:35:43 +0100

    added comments

--- exercise2/10k_diabetes.py
+++ exercise2/10k_diabetes.py
@@ -10,11 +10,13 @@ test_df  = pd.read_csv('project2_data/10k_diabetes/diab_test.csv')
 
 NaN_values = ['?', 'nan', 'Not Available', 'Not Mapped', 'None']
 
+## All integer columns
 int_columns = ['Unnamed: 0', 'time_in_hospital', 'num_lab_procedures',
        'num_procedures', 'num_medications', 'number_outpatient',
        'number_emergency', 'number_inpatient', 'number_diagnoses',
        'readmitted', 'diag_1', 'diag_2', 'diag_3']
 
+## All categorical columns
 cat_columns = ['race', 'gender', 'age', 'weight',
        'max_glu_serum',
        'A1Cresult', 'metformin', 'repaglinide', 'nateglinide',
@@ -25,6 +27,7 @@ cat_columns = ['race', 'gender', 'age', 'weight',
        'glimepiride.pioglitazone', 'metformin.rosiglitazone',
        'metformin.pioglitazone', 'change', 'diabetesMed']
 
+## All string columns
 str_columns = [ 'diag_1_desc', 'diag_2_desc', 'diag_3_desc', 'payer_code', 
         'admission_source_id', 'medical_speciality', 'admission_type_id',
         'discharge_disposition_id'
