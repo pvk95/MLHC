9005ead571552c2b630616a07c51f2bf3378255c
martin <vilmarti@student.ethz.ch>
2019-04-25 14:42:00 +0200

    changed quite a bit

--- exercise3/ptbdb.py
+++ exercise3/ptbdb.py
@@ -1,26 +1,16 @@
 import pandas as pd
 import os
 import sys
+import models
 import numpy as np
-import tensorflow as tf
-from tensorflow.python.keras import optimizers, losses, activations, models
-from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, ReduceLROnPlateau
-from tensorflow.python.keras.layers import Dense, Input, Dropout, Convolution1D, MaxPool1D, GlobalMaxPool1D, GlobalAveragePooling1D, \
-    concatenate,Reshape
-from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.layers import LSTM
-from tensorflow.python.keras.layers import CuDNNLSTM
-
-gpu = sys.argv[1]
-lstm_out = int(sys.argv[2])
 
 gpu = 0
 lstm_out = 100
 os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
 
-df_1 = pd.read_csv("heartbeat/ptbdb_normal.csv", header=None)
-df_2 = pd.read_csv("heartbeat/ptbdb_abnormal.csv", header=None)
+df_1 = pd.read_csv("exercise_data/heartbeat/ptbdb_normal.csv", header=None)
+df_2 = pd.read_csv("exercise_data/heartbeat/ptbdb_abnormal.csv", header=None)
 df = pd.concat([df_1, df_2])
 
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=1337, stratify=df[187])
@@ -31,103 +21,16 @@ X = np.array(df_train[list(range(187))].values)[..., np.newaxis]
 Y_test = np.array(df_test[187].values).astype(np.int8)
 X_test = np.array(df_test[list(range(187))].values)[..., np.newaxis]
 
-def get_model():
-    nclass = 1
-    inp = Input(shape=(187, 1))
-    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(inp)
-    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = GlobalMaxPool1D()(img_1)
-    img_1 = Dropout(rate=0.2)(img_1)
-
-    dense_1 = Dense(64, activation=activations.relu, name="dense_1")(img_1)
-    dense_1 = Dense(64, activation=activations.relu, name="dense_2")(dense_1)
-    dense_1 = Dense(nclass, activation=activations.sigmoid, name="dense_3_ptbdb")(dense_1)
-
-    model = models.Model(inputs=inp, outputs=dense_1)
-    opt = optimizers.Adam(0.001)
-
-    model.compile(optimizer=opt, loss=losses.binary_crossentropy, metrics=['acc'])
-    model.summary()
-    return model
-
-def get_lstm():
-    nclass = 1
-    inp = Input(shape=(187, 1))
-    lstm = CuDNNLSTM(lstm_out)(inp)
-    lstm = Reshape((lstm_out,1))(lstm)
-
-    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(lstm)
-    img_1 = Convolution1D(16, kernel_size=5, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(32, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = MaxPool1D(pool_size=2)(img_1)
-    img_1 = Dropout(rate=0.1)(img_1)
-    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = Convolution1D(256, kernel_size=3, activation=activations.relu, padding="valid")(img_1)
-    img_1 = GlobalMaxPool1D()(img_1)
-    img_1 = Dropout(rate=0.2)(img_1)
-
-    dense_1 = Dense(64, activation=activations.relu, name="dense_1")(img_1)
-    dense_1 = Dense(64, activation=activations.relu, name="dense_2")(dense_1)
-    dense_1 = Dense(nclass, activation=activations.sigmoid, name="dense_3_ptbdb")(dense_1)
-
-    model = models.Model(inputs=inp, outputs=dense_1)
-    opt = optimizers.Adam(0.001)
-
-    model.compile(optimizer=opt, loss=losses.binary_crossentropy, metrics=['acc'])
-    model.summary()
-    return model
-
-from sklearn.metrics import precision_recall_curve,roc_curve,auc
 metrics_df = pd.DataFrame(data=[],columns=['Name','f1_score','AUROC','AUPRC','ACC'])
 
-def train_model(model,name,metrics_df):
-    file_path = "baseline_cnn_ptbdb.h5"
-    checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    early = EarlyStopping(monitor="val_acc", mode="max", patience=5, verbose=1)
-    redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=3, verbose=2)
-    callbacks_list = [checkpoint, early, redonplat]  # early
+models_ = [
+    models.Residual_CNN('residual'),
+    models.CNN_Model('baseline'),
+    models.LSTM_Model('LSTM', epochs=1)
+]
 
-    model.fit(X, Y, epochs=1000, verbose=1, callbacks=callbacks_list, validation_split=0.1)
-    model.load_weights(file_path)
-    metrics_df = getScores(model,name,metrics_df)
-    return metrics_df
-
-def getScores(model,name,metrics_df):
-    pred_test = model.predict(X_test)
-    fpr, tpr, _ = roc_curve(Y_test, pred_test)
-    auroc = auc(fpr, tpr)
-    precision, recall, _ = precision_recall_curve(Y_test, pred_test)
-    auprc = auc(recall, precision)
-    pred_test = (pred_test > 0.5).astype(np.int8)
-    f1 = f1_score(Y_test, pred_test)
-    acc = accuracy_score(Y_test, pred_test)
-    curr_metrics = {'Name':name,'f1_score': f1, "AUROC": auroc, "AUPRC": auprc, "ACC": acc}
-    metrics_df = metrics_df.append(curr_metrics,ignore_index = True)
-    return metrics_df
-
-models = [get_model(),get_lstm()][::-1]
-model_names = ['Baseline','LSTM'][::-1]
-models = zip(models,model_names)
-for model,name in models:
-    metrics_df = train_model(model,name,metrics_df)
+for model in models_:
+    model.train(X, Y, f'{model.name}_ptbdb.h5')
+    metrics_df = model.getScores(X_test, Y_test, metrics_df)
 
 print(metrics_df)
