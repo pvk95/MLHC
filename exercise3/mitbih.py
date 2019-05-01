617d27075ce382d9b20379169408de54812bf03c
karthikp <karthikp@student.ethz.ch>
2019-05-01 20:32:24 +0200

    working mitdb

--- exercise3/mitbih.py
+++ exercise3/mitbih.py
@@ -12,14 +12,10 @@ from sklearn.metrics import roc_curve,precision_recall_curve,auc,accuracy_score,
 import matplotlib.pyplot as plt
 
 def getScores(model_name,Y_test,pred_test,metrics_df):
-    fpr, tpr, _ = roc_curve(Y_test, pred_test)
-    auroc = auc(fpr, tpr)
-    precision, recall, _ = precision_recall_curve(Y_test, pred_test)
-    auprc = auc(recall, precision)
-    pred_test = (pred_test > 0.5).astype(np.int8)
-    f1 = f1_score(Y_test, pred_test)
-    acc = accuracy_score(Y_test, pred_test)
-    curr_metrics = {'Name': model_name, 'f1_score': f1, "AUROC": auroc, "AUPRC": auprc, "ACC": acc}
+    pred_test_temp = np.argmax(pred_test,axis=-1)
+    Y_test_temp = np.argmax(Y_test,axis=-1)
+    acc = accuracy_score(Y_test_temp, pred_test_temp)
+    curr_metrics = {'Name': model_name,"ACC": acc}
     metrics_df = metrics_df.append(curr_metrics, ignore_index=True)
     return metrics_df
 
@@ -36,57 +32,44 @@ def visualize(df,title):
     plt.suptitle(title)
     #plt.show()
 
-
 gpu = 7
 lstm_out = 100
 os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-df_1 = pd.read_csv("exercise_data/heartbeat/mitbih_train.csv", header=None)
-df_2 = pd.read_csv("exercise_data/heartbeat/mitbih_test.csv", header=None)
-
-visualize(df_1, 'Training Set EEG')
-
-df = pd.concat([df_1, df_2])
-
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=1337, stratify=df[187])
+df_train = pd.read_csv("exercise_data/heartbeat/mitbih_train.csv", header=None)
+df_train = df_train.sample(frac=1)
+df_test = pd.read_csv("exercise_data/heartbeat/mitbih_test.csv", header=None)
 
 # 87556 samples
 Y = np.array(df_train[187].values).astype(np.int8)
-Y = keras.utils.to_categorical(Y)
-np.shape(Y)
 X = np.array(df_train[list(range(186))].values)[..., np.newaxis]
+Y = keras.utils.to_categorical(Y)
 
 # 21890 samples
 Y_test = np.array(df_test[187].values).astype(np.int8)
-Y_test = keras.utils.to_categorical(Y_test)
-np.shape(Y_test)
 X_test = np.array(df_test[list(range(186))].values)[..., np.newaxis]
+Y_test = keras.utils.to_categorical(Y_test)
 
-metrics_df = pd.DataFrame(data=[],columns=['Name','f1_score','AUROC','AUPRC','ACC'])
+#visualize(df_1, 'Training Set EEG')
+
+metrics_df = pd.DataFrame(data=[],columns=['Name','ACC'])
 
 models_ = [
-    sklearn.mixture.GaussianMixture(n_components=5),
-    sklearn.mixture.BayesianGaussianMixture(n_components=5),
+    #sklearn.mixture.BayesianGaussianMixture(n_components=5),
     models.LSTM_Model(outputs=5),
     RandomForestClassifier(n_jobs=-1),
     models.Residual_CNN(outputs=5),
     models.CNN_Model(outputs=5),
+    sklearn.mixture.GaussianMixture(n_components=5)
 ]
 
 params = [
-    # Gaussian mixture
-    {
-
-    },
-    # Bayesian Mixture
-    {
-
-    },
     # LSTM
     {
         'hidden': [16, 32, 64],
-        'dense': [16, 32, 64]
+        'dense': [16, 32, 64],
+        'verbose': [1]
     },
     # RandomForestClassifier
     {
@@ -104,23 +87,27 @@ params = [
         'conv3_size': [128, 256],
         'dense_size': [16, 32, 64],
     },
+    #GMM
+    {
+
+    }
 
 ]
 
 model_preds = []
 for param, model in zip(params, models_):
-    clf = RandomizedSearchCV(model, param, cv=2,n_iter=5)
+    clf = RandomizedSearchCV(model, param, cv=2,n_iter=3)
     if type(model) == RandomForestClassifier or \
         type(model) == sklearn.mixture.GaussianMixture or \
         type(model) == sklearn.mixture.BayesianGaussianMixture:
         clf.fit(np.squeeze(X), Y)
         model = clf.best_estimator_
-        model.getScores = types.MethodType(models.CNN_Model.getScores, model)
+        model.getScores = types.MethodType(models.CNN_Model.getScores_multi, model)
         _,metrics_df = model.getScores(np.squeeze(X_test), Y_test, metrics_df)
     else:
         clf.fit(X, Y)
         model = clf.best_estimator_
-        pred,metrics_df = model.getScores(X_test, Y_test, metrics_df)
+        pred,metrics_df = model.getScores_multi(X_test, Y_test, metrics_df)
         model_preds.append(pred)
     print(metrics_df)
 
@@ -131,13 +118,14 @@ model_preds = np.squeeze(model_preds)
 avg_pred = np.mean(model_preds,axis=0)
 metrics_df = getScores('Ensemble(Avg)',Y_test=Y_test,pred_test=avg_pred,metrics_df=metrics_df)
 
-## MF: not applicable for multi-class
-# #Logistic regression
-# from sklearn.linear_model import LogisticRegression
-# lg = LogisticRegression(n_jobs=-1)
-# X_lg = np.transpose(model_preds,[1,0])
-# lg.fit(X_lg,Y_test)
-# lg_pred = lg.predict_proba(X_lg)[:,0]
-# metrics_df = getScores('Ensemble(LG)',Y_test=Y_test,pred_test=lg_pred,metrics_df=metrics_df)
-#
-# print(metrics_df)
+#Logistic regression
+from sklearn.linear_model import LogisticRegression
+lg = LogisticRegression(n_jobs=-1,multi_class = 'multinomial',solver='lbfgs')
+X_lg = np.transpose(model_preds,[1,2,0])
+n_samples = X_lg.shape[0]
+X_lg = np.reshape(X_lg,[n_samples,-1])
+lg.fit(X_lg,np.argmax(Y_test,axis=-1))
+lg_pred = lg.predict_proba(X_lg)
+metrics_df = getScores('Ensemble(LG)',Y_test=Y_test,pred_test=lg_pred,metrics_df=metrics_df)
+
+print(metrics_df)
